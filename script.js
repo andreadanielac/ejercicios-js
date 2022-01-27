1f8ef2fe8c55b2f2e73237d64ff9afe4dd7d44e8
andreadanielac <ada_hack197@hackademy.mx>
2022-01-26 20:32:02 -0600

    ejercicio2 completo

--- script.js
+++ script.js
@@ -3,35 +3,33 @@ const ejercicio1 = [
   19, 88, 456, 13, 23, 24,
 ];
 
+//console.log (ejercicio1)
+
 const ejercicio2 = [
-  {
-    nombre: "Gabriel",
-    edad: 22,
-    esFamiliar: false,
+  {nombre: "Gabriel", edad: 22, esFamiliar: false,
   },
-  {
-    nombre: "Jaime",
-    edad: 15,
-    esFamiliar: true,
+  {nombre: "Jaime", edad: 15, esFamiliar: true,
   },
-  {
-    nombre: "María",
-    edad: 18,
-    esFamiliar: true,
+  {nombre: "María", edad: 18, esFamiliar: true,
   },
-  {
-    nombre: "Angel",
-    edad: 19,
-    esFamiliar: true,
+  {nombre: "Angel", edad: 19, esFamiliar: true,
   },
-  {
-    nombre: "Fer",
-    edad: 18,
-    esFamiliar: true,
+  {nombre: "Fer", edad: 18, esFamiliar: true,
   },
-  {
-    nombre: "Rachel",
-    edad: 30,
-    esFamiliar: true,
+  {nombre: "Rachel", edad: 30, esFamiliar: true,
   },
 ];
+
+for (i=0; i<ejercicio2.length;i++){
+  if (ejercicio2[i].edad >= 18 && ejercicio2[i].esFamiliar === true) {
+    console.log(`Puede pasar ${ejercicio2[i].nombre}`);
+  }
+  //console.log(ejercicio2[i].nombre)
+}
+
+
+
+
+ 
+
+
